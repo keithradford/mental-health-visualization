2c0221cd71e5ca2240affe725cc2b2486146089b
Manav Shergill <shergillmanav@uvic.ca>
2022-11-26 15:17:07 -0800

    adding modal

---
+++ src/modal.js
@@ -0,0 +1,60 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
+export function Modal({ data, isOpen, setIsOpen }) {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-[9999] overflow-y-auto"
+          onClose={() => {
+            setIsOpen(false);
+          }}
+        >
+          <div className="min-h-screen px-4 text-center bg-slate-500 bg-opacity-40">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-xl p-8 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <div className="flex flex-wrap">
+                  <Dialog.Title className="my-2 text-xl font-medium leading-6">
+                    Data for {data.country}
+                  </Dialog.Title>
+                </div>
+                <div className="flex justify-end space-x-3">
+                  <button> Cancel </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
